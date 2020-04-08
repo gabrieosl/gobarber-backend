6537cd024d6f933e7391bc5115bbd04b9373268e
gabrieosl <gabriel@Gabriels-MacBook-Pro.local>
2020-04-07 22:45:27 -0300

    push GitHub

---
+++ src/app/controllers/UserContoller.js
@@ -0,0 +1,57 @@
+import User from '../models/User';
+import File from '../models/File';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'email already used.' });
+    }
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(user.id, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar,
+    });
+  }
+}
+
+export default new UserController();
